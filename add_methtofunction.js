6a73e3db191cef99d73354dca609c24684f31a79
Brian Charlton <BCharl24@Brians-MacBook-Pro.local>
2016-04-06 11:56:51 -0400

    accessed breed method using this.breed instead of breed; added spaces in console print

--- add_methtofunction.js
+++ add_methtofunction.js
@@ -6,7 +6,7 @@ function Dog (breed) {
 // add the sayHello method to the Dog class 
 // so all dogs now can say hello
 Dog.prototype.sayHello = function(){
-    console.log("Hello this is a" + breed + "dog");
+    console.log("Hello this is a " + this.breed + " dog");
 };
 
 var yourDog = new Dog("golden retriever");
