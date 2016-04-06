b6dd18bec4fb8057b4529e0c03f4894a0b1d2d88
Brian Charlton <BCharl24@Brians-MacBook-Pro.local>
2016-04-06 12:38:06 -0400

    set ptypeType to typeof

---
+++ js_obj_baggage.js
@@ -0,0 +1,7 @@
+// what is this "Object.prototype" anyway...?
+var prototypeType = typeof Object.prototype;
+console.log(prototypeType);
+
+// now let's examine it!
+var hasOwn = 
+console.log(hasOwn);
