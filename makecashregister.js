7dd1fcc45dcc6303c157abf46ee36fe7601f52d0
Brian Charlton <BCharl24@Brians-MacBook-Pro.local>
2016-04-06 15:11:54 -0400

    added quantity parameter to scan to avoid calling scan more than once on same item

--- makecashregister.js
+++ makecashregister.js
@@ -1,41 +1,30 @@
 var cashRegister = {
     total: 0,
     add: function(itemCost) {
-        this.total += itemCost
+        this.total += itemCost 
     },
-//insert the add method here 
-    
-    
-    scan: function(item) {
+    scan: function(item, quantity) {
         switch (item) { 
-        case "eggs": 
-            this.add(0.98); 
+        case "eggs": this.add(0.98 * quantity); 
             break;
         
-        case "milk": 
-            this.add(1.23); 
+        case "milk": this.add(1.23 * quantity); 
             break;
         
-        //Add other 2 items here
-        case "magazine":
-            this.add(4.99);
+        case "magazine": this.add(4.99 * quantity);
             break;
             
-        case "chocolate":
-            this.add(0.45);
+        case "chocolate": this.add(0.45 * quantity);
             break;
-        
         }
-        return true;
     }
 };
 
-//Scan 2 eggs and 3 magazines
-cashRegister.scan("eggs");
-cashRegister.scan("eggs");
-cashRegister.scan("magazine");
-cashRegister.scan("magazine");
-cashRegister.scan("magazine");
+// scan each item 4 times
+cashRegister.scan("eggs", 4)
+cashRegister.scan("milk", 4)
+cashRegister.scan("magazine", 4)
+cashRegister.scan("chocolate", 4)
 
 //Show the total bill
 console.log('Your bill is '+cashRegister.total);
