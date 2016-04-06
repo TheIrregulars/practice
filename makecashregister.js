c9d5d5839365f9b2f4cf9f4775f1c2ca00dd2c5c
Brian Charlton <BCharl24@Brians-MacBook-Pro.local>
2016-04-06 13:29:12 -0400

    created Obj cashReg; set total to 0; changed tot to 2.99 using dotnotation

--- makecashregister.js
+++ makecashregister.js
@@ -1,6 +1,9 @@
 //Create the object called cashRegister 
 //and initialize its total property
+cashRegister = new Object();
+this.total = 0;
 
+cashRegister.total = 2.99
 
 //Using dot notation change the total property
 
